6afe023651d51bc08afee1ec5a829d7e2a1cf006
Steve Sutcliffe <sws587@mail.usask.ca>
2011-01-29 22:36:12 -0600

    Looks like a working prototype to me :), just to fill the db now

--- reviewboard/reviews/management/commands/fill-database.py
+++ reviewboard/reviews/management/commands/fill-database.py
@@ -7,52 +7,48 @@ from django.core.management.base import NoArgsCommand
 class Command(NoArgsCommand):
     help = 'Does some stuff'
 
-    option_list = BaseCommand.option_list + (
+    option_list = BaseCommand.option_list + ( 
         make_option('-u', '--users', type="int", default=None, dest='users',
             help='Specifies the number of users to add to the db'),
-        make_option('--review-requests', type="int", nargs=2, default=None,
-            dest='review-requests',
+        make_option('--review-requests', default=None, dest='review-requests',
             help='Specifies the range of review requests for each user.'),
         make_option('--diffs', default=None, dest='diffs',
             help='Specifies the range of diffs for each user.'),
         make_option('--diff-comments', default=None, dest='diff-comments',
             help='Specifies the range of review requests for each user.')
-              )
-
+        )
 
     def handle_noargs(self, **options):
         users = options.get('users', None)
         review_requests = options.get('review-requests', None)
+        diffs = options.get('diffs', None)
+        diff_comments = options.get('diff-comments', None)
 
         if users:
             self.stdout.write("The number of users=" + str(users) + "\n")
 
-####### Parses the command args as 40:50 Looking for a better way########
-#
-#        if review_requests:
-#            req_range = review_requests.partition(":")
-#            self.stdout.write("first number is="+req_range[0]+"\n" \
-#                + "second number=" + req_range[2] + "\n")
-#            try:
-#                start = int(req_range[0])
-#            except ValueError:
-#                self.stdout.write("The first arg of review-requests needs " \
-#                    + "to be an int\n")
-#                exit()
-#
-#            #If second value in range is empty, set all to first number
-#            if req_range[2]=='':
-#                self.stdout.write("Missing a second argument\n")
-#                end = start
-#            else:
-#                try:
-#                    end = int(req_range[2])
-#                except ValueError:
-#                    self.stdout.write("The second arg of review-requests " \
-#                        + "needs to be an int\n")
-#                    exit()
-#
-#            self.stdout.write("Here is the range of review-requests: " \
-#                + str(start) + " and " + str(end) + ";\n")
-#
-#
+        if review_requests:
+            req_min, req_max = self.parseCommand("review_requests", review_requests)
+            self.stdout.write("You entered a range from " + str(req_min) + \
+                    " to " + str(req_max) + "\n" )
+
+        if diffs:
+            diff_min, diff_max = self.parseCommand("diffs", diffs)
+            self.stdout.write("You entered a range from " + str(diff_min) + \
+                " to " + str(diff_max) + "\n")
+        
+        if diff_comments:
+            diff_com_min, diff_com_max = self.parseCommand("diff-comments", diff_comments)
+            self.stdout.write("You entered a range from " + str(diff_com_min) + \
+                " to " + str(diff_com_max) + "\n")
+            
+
+    def parseCommand(self, com_arg, com_string):
+        try:
+            min_range, max_range = [ int(item.strip()) for item in com_string.split(":")]
+            return min_range, max_range
+        except ValueError:
+            print "You failed to provide \"" + com_arg + "\" with two values of type int."
+            exit()
+
+
